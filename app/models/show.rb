8f6d77136cfd08f0a061c7b039427d522f987603
Cedric David <cedric.david@btopenworld.com>
2018-09-17 12:00:36 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -5,9 +5,9 @@ class Show < ActiveRecord::Base
   def self.most_popular_show
      Show.find_by(rating: Show.highest_rating)
   end
-  
+
   def self.lowest_rating
     Show.minimum(:rating)
   end
-  
+
 end
